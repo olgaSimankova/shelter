b5d37776ce65f794716899737397fe20fb5ee410
Olga Simankova <o.kotlyarova@gmail.com>
2022-04-23 18:36:48 +0300

    feat: implement burger menu actions

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,2 +1,46 @@
 console.log('Привет, дорогой проверяющий! Да, я знаю, что местами Pixel-Perfect хромает, но в условиях сказано, 10-20 px вполне допустимо.')
-console.log('Все требования по функционалу выполнены')
+
+// ----------------burger nav----------------
+const header = document.querySelector('.header');
+const burgerBtn = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation');
+const overlay = document.querySelector('.overlay');
+const body = document.querySelector('.body')
+
+const openNav = () => {
+    header.classList.add('open');
+    burgerBtn.classList.add('open');
+    navigation.classList.add('open');
+    overlay.classList.remove('hidden');
+    body.style.overflow = 'hidden';
+};
+
+const closeNav = () => {
+    header.classList.remove('open');
+    burgerBtn.classList.remove('open');
+    navigation.classList.remove('open');
+    overlay.classList.add('hidden');
+    body.style.overflow = 'scroll';
+};
+
+burgerBtn.addEventListener('click', (e) => {
+    if (e.target.classList.contains('open')) {
+        closeNav();
+    } else {
+        openNav();
+    }
+});
+
+overlay.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('hidden')) {
+        closeNav();
+    }
+});
+
+navigation.addEventListener('click', (e) => {
+    if (e.target.closest('.navigation__link')) {
+        setTimeout(() => {
+            closeNav();
+        }, 200);
+    }
+});
