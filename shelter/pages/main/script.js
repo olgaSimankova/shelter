09fe6aa504e610db7eec6cb5cb6576a1f8954b60
Olga Simankova <o.kotlyarova@gmail.com>
2022-04-25 15:29:48 +0300

    feat: implement slider random cards. OMG it was so difficult

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,4 +1,21 @@
 console.log('Привет, дорогой проверяющий!')
+import petsData from "../../assets/scripts/pets.js";
+
+let arr = [0, 1, 2, 3, 4, 5, 6, 7];
+let currentCards
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
+}
+function getRandomNum(a, b) {
+    return Math.floor(Math.random() * (b - a + 1) + a);
+}
 
 // ----------------burger nav----------------
 const header = document.querySelector('.header');
@@ -46,6 +63,7 @@ navigation.addEventListener('click', (e) => {
 });
 
 // ----------------Carousel----------------
+
 const leftBtn = document.querySelector('.arrow-left');
 const rightBtn = document.querySelector('.arrow-right');
 const slider = document.querySelector('.pets__slider');
@@ -67,5 +85,112 @@ slider.addEventListener('animationend', () => {
     slider.classList.remove("transition-left");
     leftBtn.addEventListener("click", moveLeft);
     slider.classList.remove("transition-right");
+    rightBtn.addEventListener("click", moveRight);
+})
+
+// ----------------render new slide----------------
+
+const renderPetsSlider = () => {
+    const sliderWrapper = getSliderContainer()
+    const slideCards = setSlideCards()
+
+    for (let i = 0; i < 3; i++) {
+        let newSlide
+        if (i == 0) {
+            newSlide = createNewSlide(slideCards[1])
+            newSlide.classList.add("slide-left")
+            currentCards = slideCards[1]
+        }
+        else if (i == 1) {
+            newSlide = createNewSlide(slideCards[0])
+            newSlide.classList.add("slide-active")
+        }
+        else {
+            newSlide = createNewSlide(slideCards[1])
+            newSlide.classList.add("slide-right")
+        }
+
+        sliderWrapper.append(newSlide)
+    }
+    console.log("currentCards " + currentCards)
+    return sliderWrapper
+}
+
+const getSliderContainer = () => {
+    const sliderContainer = document.querySelector(".pets__slider")
+    sliderContainer.innerHTML = ''
+    return sliderContainer
+}
+
+const setSlideCards = (arrActiveCards = []) => {
+    const width = window.innerWidth
+
+    let slideCardsSet = []
+    const newShuffledArr = shuffle(arr);
+    let randomArr = newShuffledArr.slice(0).filter(el => !arrActiveCards.includes(el))
+
+    if (width >= 1280) {
+        //cardsDisplaying = 3;
+        slideCardsSet.push(randomArr.splice(0, 3));
+        slideCardsSet.push(randomArr.splice(0, 3));
+        currentCards = slideCardsSet[1]
+
+    } else if (width >= 768 && width < 1280) {
+        //cardDisplaying = 2;
+        slideCardsSet.push(randomArr.splice(0, 2));
+        slideCardsSet.push(randomArr.splice(0, 2));
+
+    } else if (width < 768) {
+        //cardDisplaying = 1;
+        slideCardsSet.push(randomArr.splice(0, 1));
+        slideCardsSet.push(randomArr.splice(0, 1));
+    }
+    console.log(slideCardsSet)
+    return slideCardsSet
+}
+
+const createNewSlide = (petsNumbers) => {
+
+    const parentSlide = document.createElement("div")
+    parentSlide.classList.add("slide")
+
+    petsNumbers.forEach(number => {
+        let newCard = document.createElement("div");
+        newCard.classList.add("pets_card");
+        let element = petsData[number]
+
+        newCard.innerHTML =
+            `<img src="${element.img}" alt="${element.name}" class="pet-img">
+            <h4 class="pet-name">${element.name}</h4>
+            <button class="button button-bordered">Learn more</button>`
+
+        parentSlide.append(newCard);
+    })
+    return parentSlide
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    renderPetsSlider()
+})
+
+slider.addEventListener('animationend', () => {
+    slider.classList.remove('transition-left')
+    const newSlide = document.querySelector('.slide-left').innerHTML
+    document.querySelector(".slide-active").innerHTML = newSlide
+
+    const newCards = setSlideCards(currentCards)[0]
+    console.log("Номера новых 3х карточек " + newCards)
+
+    currentCards = newCards
+
+    const newFabulousSlide = createNewSlide(newCards)
+
+    let newLeftSlide = document.querySelector(".slide-left")
+    let newRightSlide = document.querySelector(".slide-right")
+
+    newLeftSlide.innerHTML = newFabulousSlide.innerHTML
+    newRightSlide.innerHTML = newFabulousSlide.innerHTML
+
+    leftBtn.addEventListener("click", moveLeft);
     rightBtn.addEventListener("click", moveRight);
 })
