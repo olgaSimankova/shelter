514880be786bbfbc31ad2b4adbf6bee7835820be
Olga Simankova <o.kotlyarova@gmail.com>
2022-07-01 18:40:15 +0300

    feat: install dependencies and add apiKey

--- Migration-to-TS/src/components/controller/appLoader.js
+++ Migration-to-TS/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'e577515cb83c4b09a7403bae5348d7ef',
         });
     }
 }
