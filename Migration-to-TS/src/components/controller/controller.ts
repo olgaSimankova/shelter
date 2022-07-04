05484e92760ed192565b3e093960f8d9a0d04659
Olga Simankova <o.kotlyarova@gmail.com>
2022-07-04 17:53:54 +0300

    feat: add types to controller.ts and rename appLoader.ts

--- Migration-to-TS/src/components/controller/controller.ts
+++ Migration-to-TS/src/components/controller/controller.ts
@@ -1,7 +1,7 @@
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: <T>(data?: T) => void) {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,13 +10,13 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: MouseEvent, callback: <T>(data?: T) => void) {
+        let target = <HTMLElement>e.target || '';
+        const newsContainer = <HTMLElement>e.currentTarget || '';
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
+                const sourceId = <string>target.getAttribute('data-source-id') || '';
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
@@ -31,7 +31,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = <HTMLElement>target.parentNode;
         }
     }
 }
