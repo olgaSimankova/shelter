4226953c23a749408a67528c28dbc26fe18a2b4e
Olga Simankova <o.kotlyarova@gmail.com>
2022-07-04 23:59:15 +0300

    fix: add correct link to fetch request

--- Migration-to-TS/src/components/controller/appLoader.ts
+++ Migration-to-TS/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'e577515cb83c4b09a7403bae5348d7ef',
         });
     }
